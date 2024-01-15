07f04363bc7ad66f2c3bfecdd81a6ecaf062baab
Michał Mucik <mucik.michal@gmail.com>
2024-01-15 13:34:09 +0100

    poprawa

--- ExercisesVariables/Program.cs
+++ ExercisesVariables/Program.cs
@@ -10,7 +10,7 @@ namespace ExercisesVariables
                 string lastName;
                 int age;    //lub uint age
                 char gender;
-                int pesel;  //lub uint pesel
+                int pesel;  //musi być 
                 int workerId; //lub uint workerId
 
                 char first = 'A';
@@ -25,12 +25,12 @@ namespace ExercisesVariables
             string sHeight = Console.ReadLine();
             Console.WriteLine("Write width of rectangle");
             string sWidth = Console.ReadLine();
-            Int32 height;
-            Int32 width;
-            Int32.TryParse(sHeight, out height);
-            Int32.TryParse(sWidth, out width);
+            int height;
+            int width;
+            int.TryParse(sHeight, out height);
+            int.TryParse(sWidth, out width);
             double c;
-            c =Math.Sqrt( Math.Pow(height, 2) + Math.Pow(width, 2) );
+            c = Math.Sqrt( Math.Pow(height, 2) + Math.Pow(width, 2) );
             Console.WriteLine($"the diagonal of the rectangle is:{c}");
           
         }
